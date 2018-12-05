cb98a09594ee2b50f0853ea3bb5b44ed4cf797dc
rw1202 <rob120216822@gmail.com>
2018-12-05 14:36:58 +0000

    first

---
+++ src/main/java/com/qa/CVProjectTrainingManager/CvProjectTrainingManagerApplication.java
@@ -0,0 +1,12 @@
+package com.qa.CVProjectTrainingManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvProjectTrainingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvProjectTrainingManagerApplication.class, args);
+	}
+}
