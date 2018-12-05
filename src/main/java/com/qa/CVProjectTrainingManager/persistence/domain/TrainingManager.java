82df47bd9c3fb7b493b3e7d40edd1b0f6cb500e8
jtbamford <rob120216822@gmail.com>
2018-12-05 15:38:00 +0000

    domain done

--- src/main/java/com/qa/CVProjectTrainingManager/persistence/domain/TrainingManager.java
+++ src/main/java/com/qa/CVProjectTrainingManager/persistence/domain/TrainingManager.java
@@ -1,5 +1,57 @@
 package com.qa.CVProjectTrainingManager.persistence.domain;
 
-public class TrainingManager {
+import java.io.Serializable;
 
+public class TrainingManager implements Serializable {
+	
+	private String username; //email
+	
+	private String password;
+
+	private String firstName;
+	
+	private String lastName;
+	
+	public TrainingManager() {
+		
+	}
+	
+	public TrainingManager(String username, String password, String firstName, String lastName) {	
+		this.username=username;
+		this.password=password;
+		this.firstName=firstName;
+		this.lastName=lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
