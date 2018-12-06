e1ef6b3436b5e578815eb82a4cb8ef3fbd115425
jtbamford <tom.bamford5@yahoo.co.uk>
2018-12-06 12:17:03 +0000

    cv class added

--- src/main/java/com/qa/CVProjectTrainingManager/persistence/domain/CV.java
+++ src/main/java/com/qa/CVProjectTrainingManager/persistence/domain/CV.java
@@ -1,5 +1,37 @@
 package com.qa.CVProjectTrainingManager.persistence.domain;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class CV {
+	
+	private Long cvID;
+	private MultipartFile[] files;
+	
+	public CV() {
+		
+	}
+	
+	public CV(Long cvID, MultipartFile[] files) {
+		this.cvID=cvID;
+		this.files=files;
+	}
+
+	public Long getCvID() {
+		return cvID;
+	}
+
+	public void setCvID(Long cvID) {
+		this.cvID = cvID;
+	}
+
+	public MultipartFile[] getFiles() {
+		return files;
+	}
+
+	public void setFiles(MultipartFile[] files) {
+		this.files = files;
+	}
+	
+	
 
 }
